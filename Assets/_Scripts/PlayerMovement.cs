c602e9a21c00add04eef3e0782805f2d9e722a65
Samed Durlanık <sametdurlanik@gmail.com>
2022-01-25 16:13:24 +0300

    Added jumping ability
    Player can jump , double or multi jumps. Jumps effected by air drag and jump force. Player get down fast according to fall multipler. Jump height effected by pressing space short or long. Same thing valid for double or multiple jumps.

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -16,8 +16,8 @@ public class PlayerMovement : MonoBehaviour
     private float _verticalDirection;
     private bool _changingDirection => (_rb.velocity.x > 0f && _horizontalDirection < 0f) || (_rb.velocity.x < 0f && _horizontalDirection > 0f);
     private bool _facingRight = true;
-    //private bool _canMove => !_wallGrab;
-    private bool _canMove => true;
+    private bool _canMove => _test;
+    private bool _test = true;
     
     [Header("Ground Collision Variables")]
     [SerializeField] private float _groundRaycastLength;
@@ -26,8 +26,21 @@ public class PlayerMovement : MonoBehaviour
     
     [Header("Layer Masks")]
     [SerializeField] private LayerMask _groundLayer;
-    [SerializeField] private LayerMask _wallLayer;
-    [SerializeField] private LayerMask _cornerCorrectLayer;
+    
+    [Header("Jump Variables")]
+    [SerializeField] private float _jumpForce = 12f;
+    [SerializeField] private float _airLinearDrag = 2.5f;
+    [SerializeField] private float _fallMultiplier = 8f;
+    [SerializeField] private float _lowJumpFallMultiplier = 5f;
+    [SerializeField] private float _downMultiplier = 12f;
+    [SerializeField] private int _extraJumps = 1;
+    [SerializeField] private float _hangTime = .1f;
+    [SerializeField] private float _jumpBufferLength = .1f;
+    private int _extraJumpsValue;
+    private float _hangTimeCounter;
+    private float _jumpBufferCounter;
+    private bool _canJump => _jumpBufferCounter > 0f && (_hangTimeCounter > 0f || _extraJumpsValue > 0 /*|| _onWall*/);
+    private bool _isJumping;
     
     
     private void Start()
@@ -39,35 +52,69 @@ public class PlayerMovement : MonoBehaviour
     {
         _horizontalDirection = GetInput().x;
         _verticalDirection = GetInput().y;
+        if (Input.GetButtonDown("Jump")) _jumpBufferCounter = _jumpBufferLength;
+        else _jumpBufferCounter -= Time.deltaTime;
     }
 
     private void FixedUpdate()
     {
         CheckCollisions();
-        
+
         if (_canMove) MoveCharacter();
         else
+            // karakteri yavaşlatarak durdurur (Aniden durmasını engeller)
+
             _rb.velocity = Vector2.Lerp(_rb.velocity,
                 (new Vector2(_horizontalDirection * _maxMoveSpeed, _rb.velocity.y)), .5f * Time.deltaTime);
         if (_onGround)
         {
+            // Yerdeyse yer sürtünmesini etkinleştirir ve zıplama koşullarını ayarlar
+            
             ApplyGroundLinearDrag();
+            _extraJumpsValue = _extraJumps;
+            _hangTimeCounter = _hangTime;
+        }
+        else
+        {
+            // Yerde değilse hava sürtünmesini etkinleştirir ve düşüş çarpanını ayarlar
+            ApplyAirLinearDrag();
+            FallMultiplier();
+            
+            // Havada kalma süresini ve zıplama koşulunu kontrol eder
+            _hangTimeCounter -= Time.fixedDeltaTime;
+            if (/*!_onWall ||*/ _rb.velocity.y < 0f /*|| _wallRun*/) _isJumping = false;
+        }
+        
+        if (_canJump)
+        {
+            Jump(Vector2.up);
         }
 
     }
 
     private Vector2 GetInput()
     {
+        // Klavye girdilerini alır WASD değerleri -1 ile 1 arasında Vector2 değer döndürür
+        
         return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
     
+    // Karakter hareketi
+    
     private void MoveCharacter()
     {
+        // Karaktere belirtilen yön ve hızlanma girdisi ile güç uygular 
+        
         _rb.AddForce(new Vector2(_horizontalDirection, 0f) * _movementAcceleration);
-
-        if (Mathf.Abs(_rb.velocity.x) > _maxMoveSpeed)
+        
+        // Karakterin hızını maxMoveSpeed ile sınırlar
+        
+       if (Mathf.Abs(_rb.velocity.x) > _maxMoveSpeed)
             _rb.velocity = new Vector2(Mathf.Sign(_rb.velocity.x) * _maxMoveSpeed, _rb.velocity.y);
     }
+    
+    
+    // Zemin sürtünmesini ayarlar
     
     private void ApplyGroundLinearDrag()
     {
@@ -81,11 +128,59 @@ public class PlayerMovement : MonoBehaviour
         }
     }
     
+    // Hava sürtünmesini ayarlar
+    private void ApplyAirLinearDrag()
+    {
+        _rb.drag = _airLinearDrag;
+    }
+    
+    // Düşüş çarpanını ayarlar (Yerçekimini havada olma koşuluna göre değiştir)
+    private void FallMultiplier()
+    {
+        if (_verticalDirection < 0f)
+        {
+            _rb.gravityScale = _downMultiplier;
+        }
+        else
+        {
+            if (_rb.velocity.y < 0)
+            {
+                _rb.gravityScale = _fallMultiplier;
+            }
+            else if (_rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                _rb.gravityScale = _lowJumpFallMultiplier;
+            }
+            else
+            {
+                _rb.gravityScale = 1f;
+            }
+        }
+    }
+    
+    
+    // Collision kontrolleri
+    
     private void CheckCollisions()
     {
         //Ground Collisions
+        
         _onGround = Physics2D.Raycast(transform.position + _groundRaycastOffset, Vector2.down, _groundRaycastLength, _groundLayer) ||
                     Physics2D.Raycast(transform.position - _groundRaycastOffset, Vector2.down, _groundRaycastLength, _groundLayer);
 
+    }
+    
+    // Zıplama Kontrolü
+    private void Jump(Vector2 direction)
+    {
+        if (!_onGround/* && !_onWall*/)
+            _extraJumpsValue--;
+
+        ApplyAirLinearDrag();
+        _rb.velocity = new Vector2(_rb.velocity.x, 0f);
+        _rb.AddForce(direction * _jumpForce, ForceMode2D.Impulse);
+        _hangTimeCounter = 0f;
+        _jumpBufferCounter = 0f;
+        _isJumping = true;
     }
 }

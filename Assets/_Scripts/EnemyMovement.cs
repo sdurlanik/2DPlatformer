3595f9cd5e9e34a9d103ea09ee47d543e090817a
Samed Durlanık <sametdurlanik@gmail.com>
2022-01-27 15:54:06 +0300

    Added URP & enemy
    Enemies can move around waypoints. İmplemented URP and settings adjusted, added some lights on screen.

--- Assets/_Scripts/EnemyMovement.cs
+++ Assets/_Scripts/EnemyMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,32 +6,54 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private Transform[] _waypoints;
-    [SerializeField] private float _speed; 
+    [SerializeField] private float _speed;
     private int _currentWaypoint = 0;
     private float _rotationSpeed;
     private float _waypointRadius = 1;
-    
+    private Animator enemyAnimator;
+
+    private void Start()
+    {
+        enemyAnimator = GetComponent<Animator>();
+    }
+
     void Update()
     {
+
+        MoveTowards();
+
+    }
+
+    private void MoveTowards()
+    {
+        // Waypointe ne kadar yaklaştığımızı algılayıp ona göre içerideki işlemleri yapar
         if (Vector2.Distance(_waypoints[_currentWaypoint].position, transform.position) < _waypointRadius)
         {
+            // Karakteri döndürüp soradaki waypoint noktasına geçiş yapar
             Flip();
             _currentWaypoint++;
+            
+            // Eğer daha fazla waypoint kalmadıysa ilk waypointe döner
             if (_currentWaypoint >= _waypoints.Length)
             {
                 _currentWaypoint = 0;
-                
+
 
             }
         }
+        
+        
+        // Sadece x eksenine doğru hareket etmesini istediğimiz için geçici bir vektör oluşturup onun x değerini karaktere veriyoruz
 
         Vector2 temp = Vector2.MoveTowards(transform.position, _waypoints[_currentWaypoint].position,
             _speed * Time.deltaTime);
 
         transform.position = new Vector2(temp.x, transform.position.y);
-        
+        enemyAnimator.SetBool("isWalking", true);
     }
 
+    
+    // Düşman karakteri tersine döndürür
     private void Flip()
     {
         transform.Rotate(0f, 180f, 0f);
